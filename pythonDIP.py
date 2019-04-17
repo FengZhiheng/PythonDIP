65983d854a8d6c65d5226e5717bc74e6f897ef28
FengZhihengLaptop <zhihengfeng5371@163.com>
2019-04-17 13:44:17 +0800

    随便写了一些内容

--- pythonDIP.py
+++ pythonDIP.py
@@ -1,1 +1,12 @@
-import 
+import os
+import sys
+import json
+import datetime
+import numpy as np
+import skimage.draw
+
+image = skimage.io.imread('D:/KerasProject/Mask_RCNN/balloon_dataset/balloon/train/53500107_d24b11b3c2_b.jpg')
+gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
+splash = gray.astype(np.uint8)
+file_name = "splash_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
+skimage.io.imsave(file_name, splash)
